f25f099d05228f319205c50fc03136fd8cb724dc
Helgi <hsigtrix@gmail.com>
2026-09-04 23:38:20 +0000

    A (non-recursive) backtracking impl.

--- regex-engine/swift/Sources/swift/main.swift
+++ regex-engine/swift/Sources/swift/main.swift
@@ -35,8 +35,61 @@ let prog = [
     Instr.Match
 ]
 
+struct Thread {
+    var pc: UInt32
+    var sp: Substring
+}
+
+func backtracking_vm(_ prog: [Instr], _ data: Substring) -> Int32 {
+    let MAX_THREADS = 1000
+    var ready: [Thread] = [] 
+    ready.append(Thread(pc: 0, sp: data))
+
+    while(!ready.isEmpty) {
+        let thread = ready.removeLast()
+        var pc = thread.pc
+        var sp = thread.sp
+
+        var done = false
+        while !done {
+            switch prog[Int(pc)] {        
+            case let .Char(c):
+                if let first = sp.first, c == first {
+                    pc += 1
+                    let nextIndex = sp.index(after: sp.startIndex)
+                    sp = sp[nextIndex...]
+                    continue
+                }
+                done = true
+            case .Match:
+                return 1
+            case let .Jump(x):
+                pc = x
+                continue
+            case let .Split(x, y):
+                if ready.count >= MAX_THREADS {
+                    print("Regex overflow")
+                    return -1
+                }
+                ready.append(Thread(pc: y, sp: sp))
+                pc = x
+                continue
+            }
+        }
+    }
+    return 0
+}
+
+
 if recursive_backtracking_vm(prog, 0, "aab") != 0 {
-    print("Match!")
+    print("Matched by the recursive backtracking VM!")
 } else {
-    print("Did not match!")
+    print("Did not match by the recursive backtracking VM!")
+}
+
+
+if backtracking_vm(prog, "aab") != 0 {
+    print("Matched by backtracking VM!")
+} else {
+    print("Did not match by the backtracking VM!")
 }
